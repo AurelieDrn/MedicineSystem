4f86064c8232ad18c36b3f4495a91629665c21a8
Aurelie Durand <aurelie.durand1@etu.univ-nantes.fr>
2017-07-11 15:23:19 +0800

    check pickup details layout tableau droit

--- app/src/main/java/com/example/lab708/tcmsystem/HomeActivity.java
+++ app/src/main/java/com/example/lab708/tcmsystem/HomeActivity.java
@@ -2,9 +2,12 @@ package com.example.lab708.tcmsystem;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
