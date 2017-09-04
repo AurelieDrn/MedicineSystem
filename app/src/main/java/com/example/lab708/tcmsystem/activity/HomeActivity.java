883cc0602a06863cb6714358ab32f52ac9bcc551
Aurelie Durand <aurelie.durand1@etu.univ-nantes.fr>
2017-09-04 11:02:14 +0800

    update : remove login

--- app/src/main/java/com/example/lab708/tcmsystem/activity/HomeActivity.java
+++ app/src/main/java/com/example/lab708/tcmsystem/activity/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.lab708.tcmsystem.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,12 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // to fix ClassLoader referenced unknown path
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
 
         //final String account = getIntent().getExtras().getString("account");
         Button shelves = (Button) findViewById(R.id.shelves_btn);
